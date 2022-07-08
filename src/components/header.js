fe74b04ee163ede3dd6568c532f36d43c6689fff
chrisPfaff <chrisPfaff10@gmail.com>
2022-07-08 18:33:34 -0500

    fake images

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import * as headerStyles from '../styles/header.module.css';
 
@@ -37,8 +38,18 @@ export default function Header() {
         sticky ? headerStyles.stickyHeader : '',
       ].join(' ')}
     >
-      <Link to="/">{meta.title}</Link>
+      <Link to="/">
+        <StaticImage
+          src="../images/logo.png"
+          alt="Logo"
+          placeholder="blurred"
+          width={150}
+          height={60}
+          style={{ display: 'block' }}
+        />
+      </Link>
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
     </header>

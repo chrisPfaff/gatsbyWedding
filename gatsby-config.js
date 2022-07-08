2122e12c7be5cfe56f412f1c4f44a2ba8a62ad72
Chris Pfaff <chrispfaff10@gmail.com>
2022-07-07 22:30:16 -0500

    added meta data

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.mysite.com",
+    title: "Old New Borrowed Blue Beauty",
+    description: "Makeup for weddings and personal events",
+    image:
+      "https://makeup-storage-media.s3.us-east-2.amazonaws.com/images/makeup_palette.jpg",
+  },
+};

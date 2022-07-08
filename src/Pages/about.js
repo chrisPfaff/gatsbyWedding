038c54a7f5f77155ff9d783b947a6b79a437991c
chrisPfaff <chrisPfaff10@gmail.com>
2022-07-08 15:26:15 -0500

    added markup to about and changed global styles for palette

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -1,11 +1,114 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
+import { imageWrapper } from '../styles/about.module.css';
 
 export default function about() {
   return (
     <Layout>
       <h1>About</h1>
+      <div className={imageWrapper}>
+        <StaticImage
+          src="https://makeup-storage-media.s3.us-east-2.amazonaws.com/images/makeup_palette.jpg"
+          alt="Makeup Pallette"
+          placeholder="blurred"
+          width={300}
+          height={300}
+        />
+      </div>
+      <p>
+        Nam repellendus perferendis est distinctio voluptas ut beatae fugiat aut
+        veritatis corporis quo similique pariatur ab dolorum ipsum quo dolor
+        consequuntur. Ea deserunt quo praesentium nostrum a temporibus expedita
+        a ipsam quas ea architecto voluptatem aut dolore fugiat aut facilis
+        animi. Ab sunt quas et voluptatem praesentium ad quibusdam sint id vitae
+        voluptas qui quis voluptatibus ea atque nostrum et similique repellat.
+        Sit inventore repellat ut perspiciatis amet et quia nobis et galisum
+        ullam! Aut suscipit aliquam et voluptatem doloribus aut magnam velit.
+      </p>
+      <p>
+        Et quae placeat eos aspernatur iure vel ratione vitae est fuga ducimus
+        et provident voluptatem! Ut fuga totam est dicta dolore ea ipsa minima
+        At error dolores. Non dolor eius in accusamus deleniti et dolorem quidem
+        aut voluptatem suscipit sit consequatur deserunt nam eaque sit
+        exercitationem omnis. Qui voluptatibus nihil et molestiae optio quo quae
+        illo quidem laboriosam aut quos ipsa est labore animi? Aut tempora
+        galisum At omnis omnis hic autem accusantium At rerum quod non voluptas
+        totam ut repellat illo et enim ducimus? Eum autem voluptatem sed
+        voluptates nesciunt ut Quis fuga sed dolorem suscipit. Qui labore
+        provident et maiores odit 33 laboriosam assumenda hic impedit nulla qui
+        nihil iusto 33 ducimus nihil hic rerum galisum. Nam perferendis ut
+        incidunt nobis est cupiditate eaque vel sint saepe vel voluptates quasi
+        ut reiciendis eius? Sed provident repudiandae eos perspiciatis minima
+        eum sapiente veniam vel aperiam voluptatem aut internos consequatur non
+        enim laborum.
+      </p>
+      <p>
+        Quo assumenda galisum cum natus corrupti et odit quos in repellendus
+        nulla qui molestiae quidem hic accusantium veniam qui consequatur ullam!
+        Qui eaque ducimus et expedita rerum aut facilis provident aut aliquam
+        aperiam qui dolorem consequatur et internos minima et voluptatibus
+        quaerat. Sed ducimus quia eos assumenda molestias est vero molestiae quo
+        maxime autem. Est facilis quos rem quas aperiam eos aperiam fugit id
+        quia dicta non fugiat facilis. Aut libero ducimus id nostrum magni et
+        unde dolor est perspiciatis neque aut aperiam similique est perspiciatis
+        recusandae et odio esse.{' '}
+      </p>
+      <p>
+        Nam repellendus perferendis est distinctio voluptas ut beatae fugiat aut
+        veritatis corporis quo similique pariatur ab dolorum ipsum quo dolor
+        consequuntur. Ea deserunt quo praesentium nostrum a temporibus expedita
+        a ipsam quas ea architecto voluptatem aut dolore fugiat aut facilis
+        animi. Ab sunt quas et voluptatem praesentium ad quibusdam sint id vitae
+        voluptas qui quis voluptatibus ea atque nostrum et similique repellat.
+        Sit inventore repellat ut perspiciatis amet et quia nobis et galisum
+        ullam! Aut suscipit aliquam et voluptatem doloribus aut magnam velit.
+      </p>
+      <p>
+        Et quae placeat eos aspernatur iure vel ratione vitae est fuga ducimus
+        et provident voluptatem! Ut fuga totam est dicta dolore ea ipsa minima
+        At error dolores. Non dolor eius in accusamus deleniti et dolorem quidem
+        aut voluptatem suscipit sit consequatur deserunt nam eaque sit
+        exercitationem omnis. Qui voluptatibus nihil et molestiae optio quo quae
+        illo quidem laboriosam aut quos ipsa est labore animi? Aut tempora
+        galisum At omnis omnis hic autem accusantium At rerum quod non voluptas
+        totam ut repellat illo et enim ducimus? Eum autem voluptatem sed
+        voluptates nesciunt ut Quis fuga sed dolorem suscipit. Qui labore
+        provident et maiores odit 33 laboriosam assumenda hic impedit nulla qui
+        nihil iusto 33 ducimus nihil hic rerum galisum. Nam perferendis ut
+        incidunt nobis est cupiditate eaque vel sint saepe vel voluptates quasi
+        ut reiciendis eius? Sed provident repudiandae eos perspiciatis minima
+        eum sapiente veniam vel aperiam voluptatem aut internos consequatur non
+        enim laborum.
+      </p>
+      <p>
+        Nam repellendus perferendis est distinctio voluptas ut beatae fugiat aut
+        veritatis corporis quo similique pariatur ab dolorum ipsum quo dolor
+        consequuntur. Ea deserunt quo praesentium nostrum a temporibus expedita
+        a ipsam quas ea architecto voluptatem aut dolore fugiat aut facilis
+        animi. Ab sunt quas et voluptatem praesentium ad quibusdam sint id vitae
+        voluptas qui quis voluptatibus ea atque nostrum et similique repellat.
+        Sit inventore repellat ut perspiciatis amet et quia nobis et galisum
+        ullam! Aut suscipit aliquam et voluptatem doloribus aut magnam velit.
+      </p>
+      <p>
+        Et quae placeat eos aspernatur iure vel ratione vitae est fuga ducimus
+        et provident voluptatem! Ut fuga totam est dicta dolore ea ipsa minima
+        At error dolores. Non dolor eius in accusamus deleniti et dolorem quidem
+        aut voluptatem suscipit sit consequatur deserunt nam eaque sit
+        exercitationem omnis. Qui voluptatibus nihil et molestiae optio quo quae
+        illo quidem laboriosam aut quos ipsa est labore animi? Aut tempora
+        galisum At omnis omnis hic autem accusantium At rerum quod non voluptas
+        totam ut repellat illo et enim ducimus? Eum autem voluptatem sed
+        voluptates nesciunt ut Quis fuga sed dolorem suscipit. Qui labore
+        provident et maiores odit 33 laboriosam assumenda hic impedit nulla qui
+        nihil iusto 33 ducimus nihil hic rerum galisum. Nam perferendis ut
+        incidunt nobis est cupiditate eaque vel sint saepe vel voluptates quasi
+        ut reiciendis eius? Sed provident repudiandae eos perspiciatis minima
+        eum sapiente veniam vel aperiam voluptatem aut internos consequatur non
+        enim laborum.
+      </p>
     </Layout>
   );
 }
